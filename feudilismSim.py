85c30ee2ecb89398029e423ab104383d2004b177
skull32222 <matthew.pfannenstiel@blackburn.edu>
2016-01-21 20:05:54 -0600

    Functions updated
    need to do debugging

--- feudilismSim.py
+++ feudilismSim.py
@@ -1,12 +1,15 @@
+from random import random
+
 import golly as g
+NULL = 999
 
-
-def calcwealth(l, j, w):
+def calcwealth(fiefs, jnum, grain):
     i = 0
-    while i < len(l[j]):
-        w[j] += l[j][i[3]]
+    while i < len(fiefs[j]):
+        if (fiefs[jnum][i][0] and fiefs[jnum][i][1]) != NULL:
+            grain[jnum] += fiefs[jnum][i][2]
         i += 1
-    return w
+    return grain
 
 
 def getvonnuemann(x, y):
@@ -18,39 +21,69 @@ def getvonnuemann(x, y):
     return c
 
 
-def combat(a, d):
+def combat(attacker, defender, ja, jb):
 
 
-def getborders(l, j):
-    b =[len(l[j])]
-    g.note(str(len(b)))
+
+    return winner
+
+def getborders(fiefs, jnum, size):
+    b =[size]
     x = 0
+    u = 0
     while x < (len(b) + 1):
-        
+        if (fiefs[jnum][x][0] and fiefs[jnum][x][1]) != NULL:
+            c = getvonnuemann(fiefs[jnum][x][0], fiefs[jnum][x][1])
+            y = 0
+            while y < len(c + 1):
+                if jnum != c[y][1]:
+                    b[u] = c[y]
+                    u += 1
+                y +=1
         x += 1
+    return b
 
-def placeserfs(l):
+
+def placeserfs(fiefs, landUnits, runs, size):
+    #places serfs on random landUnits within each feif every 2 years
+    f = len(fiefs)
+    if runs%2 ==0:
+        x = 0
+        while x < (f+1):
+            g = random(size)
+            if (fiefs[x][g][0] and fiefs[x][g][1])!= NULL:
+                landUnits[fiefs[x][g][0]][fiefs[x][g][1]][2] = 100
+            x +=1
+    return landUnits
 
 
-def main(wea, h, l, f, d, g):
+def main(width, height, landunits, fiefs, grain):
+    lords = [10]
+
+    return 0
 
 
 try:
     g.new("Feudalism Simulation")
+    global NULL
     #User entered parameters for testing
     numRuns = int(g.getstring("Enter a number of iterations", "5000"))
-    lords = int(g.getstring("Enter a number of Lords", "10"))
+    lords = 10
     width = lords * 10
     height = lords * 10
     #Lords [id of lord][array of land unit pairs in [0] is x [1] is y][wealth of lord]
-    fiefs = [[0 for x in range(lords)]for x in range(width * height)]
+    fiefs = [[NULL for x in range(lords)]for x in range(width * height)]
     grain = [0 for x in range(lords)]
     #Array for storing the states and owners of the landunits
     landUnits = [[0 for x in range(width)] for x in range(height)]
     x = 0
-    i = 0
+    runs = 0
     p = 0
     #Goes through each land unit to give it an initial state and owner
+    #List of 3 at each x, y coordinate are
+    #State in the first position
+    #Variable j represents the Lord Number that owns a landUnit in the second
+    #Grain per year in the third
     #State 1 is farmable land
     #State 2 is occupied and normal production land
     #State 3 is occupied and underproducing land
@@ -61,25 +94,25 @@ try:
         j = 1
         while y < height:
             if y < (j * 10):
-                landUnits[x][y] = [1, j, 100]
-                fiefs[j][p] = [x, y]
+                landUnits[x][y] = [1, j-1, 0]
+                fiefs[j-1][p] = [x, y]
                 p += 1
             else:
                 if j < (fiefs+1):
                     j += 1
-                    landUnits[x][y] = [1, j, 100]
-                    fiefs[j][p] = [x, y]
+                    landUnits[x][y] = [1, j-1, 0]
+                    fiefs[j-1][p] = [x, y]
                     p += 1
-            g.show("Expected: " + str(j) + ", Actual for " + str(x) + ", " + str(y) + ": " + str(landUnits[x][y][2]))
-            g.note("fiefs " + str(j) + " at " + str(p) + " should be [" + str(x) + ", " + str(y) + "]")
+            g.show("Expected: " + str(j-1) + ", Actual for " + str(x) + ", " + str(y) + ": " + str(landUnits[x][y][1]))
+            g.note("fiefs " + str(j-1) + " at " + str(p) + " should be [" + str(x) + ", " + str(y) + "]")
             g.setcell(x, y, 1)
             y += 1
         x += 1
     g.update()
-    while i < numRuns:
-        g.show("In Main " + str(i))
-        main(width, height, landUnits, fiefs, lords, grain)
-        i += 1
+    while runs < numRuns:
+        g.show("In Main " + str(runs))
+        main(width, height, landUnits, fiefs, grain, runs)
+        runs += 1
 
 
 finally:
